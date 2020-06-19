0e6ad7e48d69daa9531923a738c5bb97fe0ca2f9
Maciej Jedrzejewski <maciej.jedrzejewski@rtbhouse.com>
2020-06-19 12:14:36 +0200

    Lift 1024 open files limit (switch to curl polling API) https://github.com/transmission/transmission/pull/893

--- libtransmission/fdlimit.c
+++ libtransmission/fdlimit.c
@@ -12,11 +12,6 @@
 #include <inttypes.h>
 #include <string.h>
 
-#ifndef _WIN32
- #include <sys/time.h> /* getrlimit */
- #include <sys/resource.h> /* getrlimit */
-#endif
-
 #include "transmission.h"
 #include "error.h"
 #include "error-types.h"
@@ -364,23 +359,6 @@ ensureSessionFdInfoExists (tr_session * session)
       i = tr_new0 (struct tr_fdInfo, 1);
       fileset_construct (&i->fileset, FILE_CACHE_SIZE);
       session->fdInfo = i;
-
-#ifndef _WIN32
-      /* set the open-file limit to the largest safe size wrt FD_SETSIZE */
-      struct rlimit limit;
-      if (!getrlimit (RLIMIT_NOFILE, &limit))
-        {
-          const int old_limit = (int) limit.rlim_cur;
-          const int new_limit = MIN (limit.rlim_max, FD_SETSIZE);
-          if (new_limit != old_limit)
-            {
-              limit.rlim_cur = new_limit;
-              setrlimit (RLIMIT_NOFILE, &limit);
-              getrlimit (RLIMIT_NOFILE, &limit);
-              tr_logAddInfo ("Changed open file limit from %d to %d", old_limit, (int)limit.rlim_cur);
-            }
-        }
-#endif
     }
 }
 
